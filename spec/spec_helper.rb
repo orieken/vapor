c672d5746f1527a80e3284bfbadc012cc746e3c4
Pedro Nascimento <pnascimento@gmail.com>
2012-11-27 03:37:19 -0200

    Making :vcr work.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,6 +11,7 @@ VCR.configure do |config|
   config.cassette_library_dir     = 'spec/cassettes'
   config.hook_into                :webmock
   config.default_cassette_options = { :record => :new_episodes }
+  config.configure_rspec_metadata!
 end
 
 RSpec.configure do |config|
