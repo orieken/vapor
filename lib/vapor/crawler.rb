40f340408ad16ed3d9234fc287bb79089957ebf9
Pedro Nascimento <pnascimento@gmail.com>
2012-12-03 13:56:11 -0200

    small refactor.

--- lib/vapor/crawler.rb
+++ lib/vapor/crawler.rb
@@ -3,10 +3,14 @@ require 'nokogiri'
 module Vapor
   class Crawler
     def games_for(user)
-      doc = client.get("#{user.profile_url}games?xml=1").body
-      Nokogiri::Slop(doc).gamesList.games.elements
+      doc = fetch("#{user.profile_url}games")
+      doc.gamesList.games.elements
     end
     private
+    def fetch(url)
+      doc = client.get("#{url}?xml=1").body
+      Nokogiri::Slop(doc)
+    end
     def client
       @client ||= HTTPClient.new
     end
