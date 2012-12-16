7eb4cf60d8d54e5cf63647b486fd2bf76a3ca46f
Pedro Nascimento <pnascimento@gmail.com>
2012-12-16 03:09:44 -0200

    Making cassettes work without a proper steam API key.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,7 +4,7 @@ require 'vcr'
 VCR.configure do |config|
   config.cassette_library_dir = 'spec/cassettes'
   config.hook_into :webmock
-  config.default_cassette_options = { :record => :new_episodes }
+  config.default_cassette_options = { record: :new_episodes, match_requests_on: [:path]}
   config.configure_rspec_metadata!
 end
 
